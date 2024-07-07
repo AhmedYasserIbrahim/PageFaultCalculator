9259f85826064f01b554777c0d7e9c697a37384a
AhmedYasserIbrahim <159584129+AhmedYasserIbrahim@users.noreply.github.com>
2024-07-07 14:20:30 +0300

    Updated code file
    Added the LRU algorithm to the code

--- program.py
+++ program.py
@@ -13,6 +13,37 @@ def FIFO(pages, size):
             faults = faults+1 #increment the fault number if the page is not in the cache for both cases
 
     return faults
+
+def LRU(pages, size):
+    faults =0
+    cache = []
+    safe = [] #Use an array that keeps track of the pages that will not be removed from cache
+    for i in range(len(pages)): #Easier to use indecies
+        safe = [] #Reset the array in every iteration
+        if pages[i] not in cache:
+            if len(cache)<size:
+                cache.append(int(pages[i]))
+            
+            else:
+                for k in range(i-1,-1,-1): #Iterate from the current index in pages until 0
+                    if pages[k] not in safe: #Add pages to array safe starting from page before current page
+                        safe.append(pages[k]) 
+                        if len(safe) == size:
+                            break
+                cache.remove(safe[size-1]) #Remove the last page added to the safe from array cache
+                cache.append(int(pages[i])) #Add the new page to the cache
+            
+            faults=faults+1
+
+    return faults
+                        
+                
+
+
+
+
+
+
     
 #**Main code**
 print("Please enter the number of pages:")
@@ -24,5 +55,7 @@ print("Please enter the pages(items): ")
 for i in range(pageNo): #Fill the pages list with the page numbers
     pages.append(int(input())) #Make sure to convert from String to int before populating the list
 
-print("The number of page faults for FIFO algorithm is: " + str(FIFO(pages, cacheSize)))
+print("The number of page faults for FIFO algorithm is: " + str(LRU(pages, cacheSize)))
+#Adding the hit and miss ratio would be a good feature
+
 
